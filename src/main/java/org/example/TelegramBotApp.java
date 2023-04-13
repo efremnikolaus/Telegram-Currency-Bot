b42e28c1e602e7dd4f18113f499bbc4ff126bbcb
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-04-13 17:46:30 +0200

    connect to Telegram

--- src/main/java/org/example/TelegramBotApp.java
+++ src/main/java/org/example/TelegramBotApp.java
@@ -1,21 +1,11 @@
 package org.example;
 
-import org.example.UI.PrettyPrintCurrencyService;
-import org.example.currency.CurrencyService;
-import org.example.currency.dto.Currency;
-import org.example.currency.PrivatBankCurrencyService;
+import org.example.telegramm.TelegramBotService;
 
 import java.io.IOException;
 
 public class TelegramBotApp {
     public static void main(String[] args) throws IOException {
-        CurrencyService currencyService = new PrivatBankCurrencyService();
-
-        Currency currency = Currency.USD;
-        double rate = currencyService.getRate(currency);
-
-        String prettyText = new PrettyPrintCurrencyService().convert(rate, currency);
-
-        System.out.println(prettyText);
+        TelegramBotService botService = new TelegramBotService();
     }
 }
---
+++ src/main/java/org/example/telegramm/command/StartCommand.java
@@ -0,0 +1,17 @@
+package org.example.telegramm.command;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+public class StartCommand extends BotCommand {
+    public StartCommand() {
+        super("start", "With this command you can start the Bot");
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
+        System.out.println("Start pressed!");
+    }
+}
