1b8f32411df4c1ac8b6a97b52fe255c26e97597c
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-04-13 19:12:26 +0200

    new UI for Telegram Bot Tolik :)

--- src/main/java/org/example/telegramm/command/StartCommand.java
+++ src/main/java/org/example/telegramm/command/StartCommand.java
@@ -1,9 +1,24 @@
 package org.example.telegramm.command;
 
+import org.example.currency.dto.Currency;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Document;
 import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StartCommand extends BotCommand {
     public StartCommand() {
@@ -12,6 +27,26 @@ public class StartCommand extends BotCommand {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
-        System.out.println("Start pressed!");
+        String text = "What currency do you want to know: ";
+
+        SendMessage message = new SendMessage();
+        message.setText(text);
+        message.setChatId(Long.toString(chat.getId()));
+
+        List<InlineKeyboardButton> buttons = Stream.of(Currency.USD, Currency.EUR, Currency.UAH)
+                .map(Enum::name)
+                .map(it -> InlineKeyboardButton.builder().text(it).callbackData(it).build())
+                .toList();
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
+                .builder()
+                .keyboard(Collections.singleton(buttons))
+                .build();
+        message.setReplyMarkup(keyboard);
+
+        try {
+            absSender.execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
